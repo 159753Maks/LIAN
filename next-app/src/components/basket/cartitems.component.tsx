3f603fe0ca2c8f168cd8982cbd1285c7e0e25c0c
Dmytro Bobrov <ext.dmytro.bobrov@palqee.com>
2024-06-24 20:15:12 +0300

    fix(basket): fix styles

--- next-app/src/components/basket/cartitems.component.tsx
+++ next-app/src/components/basket/cartitems.component.tsx
@@ -2,6 +2,18 @@ import React from 'react';
 
 import { ICartItem } from '@/pages/basket';
 
+const ProductInfo = ({
+  label,
+  value,
+}: {
+  label: string;
+  value: string | number;
+}) => (
+  <div style={{ display: 'flex', alignItems: 'center' }}>
+    <p>{label}</p> <p>{value}</p>
+  </div>
+);
+
 export default function CartItemComponent({
   products,
   deleteProductOnclick,
@@ -9,35 +21,36 @@ export default function CartItemComponent({
   products: ICartItem[];
   deleteProductOnclick: (uid: string) => void;
 }) {
-  console.log(products);
   return (
     <div className="mt-20 pt-10">
       <p className="font-bold">Товари в кошику</p>
-      {products.length === 0 && <p>Кошик порожній</p>}
-
-      {products.map(product => (
-        <div className="flex flex-row justify-between items-center my-5">
-          <div className="w-24 h-24 mr-5">
-            <img
-              src={product.images[0].url || '/no-image.png'}
-              alt="Фото товара"
-            />
+      {products.length === 0 ? (
+        <p>Кошик порожній</p>
+      ) : (
+        products.map(product => (
+          <div className="flex flex-row justify-between items-center my-5">
+            <div className="w-24 h-24 mr-5">
+              <img
+                src={product.images[0].url || '/no-image.png'}
+                alt="Фото товара"
+              />
+            </div>
+            <div className="w-3/5">
+              <ProductInfo label="Название товара :" value={product.title} />
+              <ProductInfo label="Ціна: " value={product.cost} />
+              <ProductInfo label="Кількість: " value={product.count} />
+            </div>
+            <div className="text-right">
+              <button
+                className="bg-red-500 text-white py-2 px-4 border-none cursor-pointer"
+                onClick={() => deleteProductOnclick(product.uid)}
+              >
+                Видалити
+              </button>
+            </div>
           </div>
-          <div className="">
-            <h3>Название товара :</h3> <p>{product.title}</p>
-            <p>Ціна: </p> <p>{product.cost}</p>
-            <p>Кількість: </p> {product.count}
-          </div>
-          <div className="text-right">
-            <button
-              className="bg-red-500 text-white py-2 px-4 border-none cursor-pointer"
-              onClick={() => deleteProductOnclick(product.uid)}
-            >
-              Видалити
-            </button>
-          </div>
-        </div>
-      ))}
+        ))
+      )}
     </div>
   );
 }
